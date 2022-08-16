0aa600b435e119b2179db58d30f412753ec27eff
Cristian Josue Herrera Bermúdez <USSS108822@ugb.edu.sv>
2022-08-16 15:36:50 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semana_4_proyecto_de_un_formulario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
